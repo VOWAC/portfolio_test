046dcc4454102e48ad3844a1ae2b235d26aff184
Yukiha-ka <107377086+Yukiha-ka@users.noreply.github.com>
2023-02-03 21:29:36 +0900

    bg add

---
+++ components/bg.js
@@ -0,0 +1,27 @@
+import React from "react";
+// import Image from "next/image";
+// import texture from "public/texture.jpg";
+// import styles from "../styles/bg.module.scss";
+// import logo from "../public/YukihaLogo.svg"
+
+const Bg = () => {
+  return (
+    <>
+      {/* <Image
+        className={styles.bg}
+        src={texture}
+        layout="fill"
+        objectFit="cover"
+      />
+      <Image
+        className={styles.logo}
+        src={logo}
+        layout="fill"
+        // objectFit="cover"
+        //
+      /> */}
+    </>
+  );
+};
+
+export default Bg;
