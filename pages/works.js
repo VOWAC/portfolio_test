d2721833e02aa0267660e65ddf9860415912655b
Yukiha-ka <107377086+Yukiha-ka@users.noreply.github.com>
2023-02-03 23:56:13 +0900

    works add

--- pages/works.js
+++ pages/works.js
@@ -1,9 +1,14 @@
 import React from "react";
-import Header from "../components/header"
+import Header from "../components/header";
 import Container from "../components/container";
 import styles from "../styles/works.module.scss";
 import Link from "next/link";
 import { motion } from "framer-motion";
+import Image from "next/image";
+import Works1 from "../public/works/works1.png"
+import Works2 from "../public/works/works2.png"
+import Works3 from "../public/works/works3.png"
+import Works4 from "../public/works/works4.png"
 
 const Works = () => {
   return (
@@ -29,6 +34,12 @@ const Works = () => {
           </nav>
         </div>
       </Container>
+      <div className={styles.imgsList}>
+            <Image className={styles.imgs} src={Works1} width={330} height={200}></Image>
+            <Image className={styles.imgs} src={Works2} width={330} height={200}></Image>
+            <Image className={styles.imgs} src={Works3} width={330} height={200}></Image>
+            <Image className={styles.imgs} src={Works4} width={330} height={200}></Image>
+        </div>
     </motion.div>
   );
 };
