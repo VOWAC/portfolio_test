e7d31143c817ede6929e1e9a421fb0434717595b
Yukiha-ka <107377086+Yukiha-ka@users.noreply.github.com>
2023-02-04 18:13:46 +0900

    works add

--- pages/works.js
+++ pages/works.js
@@ -5,10 +5,10 @@ import styles from "../styles/works.module.scss";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import Works1 from "../public/works/works1.png"
-import Works2 from "../public/works/works2.png"
-import Works3 from "../public/works/works3.png"
-import Works4 from "../public/works/works4.png"
+import Works1 from "../public/works/works1.png";
+import Works2 from "../public/works/works2.png";
+import Works3 from "../public/works/works3.png";
+import Works4 from "../public/works/works4.png";
 
 const Works = () => {
   return (
@@ -33,13 +33,29 @@ const Works = () => {
             </Link>
           </nav>
         </div>
+        <div className={styles.imgsList}>
+        <Link href="https://test-works1.vercel.app/" target="_blank" className={styles.imgs}>
+          <Image src={Works1} width={330} height={200} alt="works1"></Image>
+        </Link>
+        <Link href="https://test-works2.vercel.app/" target="_blank" className={styles.imgs}>
+          <Image
+            src={Works2}
+            width={330}
+            height={200}
+            alt="works2"
+          ></Image>
+        </Link>
+        <Link href="https://test-works3.vercel.app/"  target="_blank" className={styles.imgs}>
+          <Image
+            src={Works3}
+            width={330}
+            height={200}
+            alt="works3"
+          ></Image>
+        </Link>
+      </div>
       </Container>
-      <div className={styles.imgsList}>
-            <Image className={styles.imgs} src={Works1} width={330} height={200} alt="works1"></Image>
-            <Image className={styles.imgs} src={Works2} width={330} height={200} alt="works2"></Image>
-            <Image className={styles.imgs} src={Works3} width={330} height={200} alt="works3"></Image>
-            <Image className={styles.imgs} src={Works4} width={330} height={200} alt="works4"></Image>
-        </div>
+     
     </motion.div>
   );
 };
